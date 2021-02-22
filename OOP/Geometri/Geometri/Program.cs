50a1e6911319f1dc4940627e7c5b2d9c8c00b983
SnitzelKlaus <SnitzelKlaus@gmail.com>
2021-02-22 10:24:16 +0100

    Update + Geometri assignment
    Update + Geometri assignment

---
+++ OOP/Geometri/Geometri/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Side: ");
+            Square square = new Square(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Perimeter: " + square.Perimeter());
+            Console.WriteLine("Area: " + square.Area());
+
+            Console.Write("\nSide: ");
+            Square secondSquare = new Square(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Perimeter: " + secondSquare.Perimeter());
+            Console.WriteLine("Area: " + secondSquare.Area());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/Geometri/Geometri/Square.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometri
+{
+    public class Square
+    {
+        private double Side { get; set; }
+
+        public Square(double Side)
+        {
+            this.Side = Side;
+        }
+
+        public double Perimeter()
+        {
+            return 4 * Side;
+        }
+
+        public double Area()
+        {
+            return Side * Side;
+        }
+    }
+}
