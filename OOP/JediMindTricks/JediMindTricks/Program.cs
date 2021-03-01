8193efb9b4381b40ac2002d141af093fb6a11e25
SnitzelKlaus <SnitzelKlaus@gmail.com>
2021-03-01 08:46:05 +0100

    Assignemt 'JediMindTricks'
    Done

--- OOP/JediMindTricks/JediMindTricks/Program.cs
+++ OOP/JediMindTricks/JediMindTricks/Program.cs
@@ -10,15 +10,19 @@ namespace JediMindTricks
         {
             Dictionary<string, int> people = new Dictionary<string, int>();
 
+            //Adds values to people dictionary
             people.Add("Benjamin", 20);
             people.Add("Han", 23);
             people.Add("Nicki", 19);
 
             Console.WriteLine($"First person: {people.First()}");
 
+            //Removes a value from people dictionary
             people.Remove("Han");
 
             Console.WriteLine();
+
+            //Prints out values from people dictionary
             foreach (KeyValuePair<string, int> pair in people)
             {
                 Console.WriteLine($"Name: {pair.Key} Age: {pair.Value}");
