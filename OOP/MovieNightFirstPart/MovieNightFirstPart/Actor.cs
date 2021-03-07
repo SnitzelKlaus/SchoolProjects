e9d935727fdaf3884aa7717efa53f282264b2481
SnitzelKlaus <SnitzelKlaus@gmail.com>
2021-03-07 17:41:16 +0100

    Updated
    Finished Part one

--- OOP/MovieNightFirstPart/MovieNightFirstPart/Actor.cs
+++ OOP/MovieNightFirstPart/MovieNightFirstPart/Actor.cs
@@ -6,6 +6,12 @@ namespace MovieNightFirstPart
 {
     public class Actor
     {
+        public Actor(string actor_firstname, string actor_lastname)
+        {
+            Actor_FirstName = actor_firstname;
+            Actor_LastName = actor_lastname;
+        }
+
         public Actor(int actor_sid, string actor_firstname, string actor_lastname)
         {
             Actor_SID = actor_sid;
