c057b915579584fa6f666e1840fd3c8937278ea6
SnitzelKlaus <SnitzelKlaus@gmail.com>
2021-03-07 21:23:21 +0100

    Updated
    Done.

--- OOP/GeometriExtended/GeometriExtended/Program.cs
+++ OOP/GeometriExtended/GeometriExtended/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace GeometriExtended
+namespace Geometri
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Side: ");
+            Square square = new Square(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Perimeter: " + square.Perimeter());
+            Console.WriteLine("Area: " + square.Area());
+
+            Console.Write("\nSide: ");
+            Square secondSquare = new Square(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Perimeter: " + secondSquare.Perimeter());
+            Console.WriteLine("Area: " + secondSquare.Area());
+
+            Console.ReadKey();
         }
     }
 }
---
+++ OOP/GeometriExtended/GeometriExtended/Square.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometri
+{
+    public class Square
+    {
+        private double Side { get; set; }
+
+        public Square(double side)
+        {
+            Side = side;
+        }
+
+        public double Perimeter()
+        {
+            return 4 * Side;
+        }
+
+        public double Area()
+        {
+            return Side * Side;
+        }
+    }
+}
